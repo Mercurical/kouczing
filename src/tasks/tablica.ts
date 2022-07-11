aeeedadce5dc3cb288b405570257fd2f75c55f7e
henoth <henocik997@gmail.com>
2022-07-11 11:04:07 +0200

    array 2.0

--- src/tasks/tablica.ts
+++ src/tasks/tablica.ts
@@ -1,6 +1,9 @@
-let array: number[]=[1,2,3,4,5];
+function parzyste(array: number){
+    return (array%2==0)
+}
 let sum: number =0;
+const array: number[]=[1,2,3,4,5].filter(parzyste);
 for(let i=0;i<array.length;i++){
-    if (array[i] %2 ==0) sum=sum+array[i]; 
+    sum=sum+array[i]
 }
 console.log(sum);
