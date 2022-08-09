72f586c849a1c60caf396588069b94c667ad5a65
Artur Nowicki <arti222a@o2.pl>
2022-08-09 17:45:21 +0200

    feat: eslint with facebook strict rules added

--- src/basics/slowa.ts
+++ src/basics/slowa.ts
@@ -1,14 +1,9 @@
-// // zdanie zrobic palindrom i anagram
-function palindrome(cokolwiek: string ) { 
-    const odwrocone = cokolwiek.split('').reverse().join('');
+export function palindrome (cokolwiek: string) { 
+    const odwrocone = cokolwiek.split("").reverse().join("");
     return cokolwiek === odwrocone;
 }
-const slowo = 'asd';
-console.log(palindrome(slowo));
 
-function anagram (slowoA: string , slowoB: string){
-    return slowoA.split('').sort().join('') === slowoB.split('').sort().join('');
+export function anagram (slowoA: string, slowoB: string) {
+    return slowoA.split("").sort().join("") === 
+        slowoB.split("").sort().join("");
 }
-const slowo1 = "niedziela";
-const slowo2 = "dzielenia";
-console.log(anagram(slowo1,slowo2));
